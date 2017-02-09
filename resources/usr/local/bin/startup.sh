60f629af7df42f2f38b0604386291a3c36e9cf21
Chris Evans <cae.dev@gmail.com>
2017-02-09 10:36:48 +0000

    Fixed some issues with startup when admin user already exists.

--- resources/usr/local/bin/startup.sh
+++ resources/usr/local/bin/startup.sh
@@ -3,30 +3,34 @@
 set -e
 
 # Go to grav home
-GRAV_HOME=/var/www/grav-admin
+export GRAV_HOME=/var/www/grav-admin
 echo "[ INFO ] Grav home set to" $GRAV_HOME
 cd $GRAV_HOME
 
-if [ -e /var/www/grav-admin/grav-configured ]; then
+# Set the correct permissions
+echo "[ INFO ] Setting folder permissions on grav home"
+chown -R www-data:www-data $GRAV_HOME
+
+if [ -e $GRAV_HOME/grav-configured ]; then
 	echo "[ INFO ] Grav already configured"
 else
 
     if [ -z $ADMIN_USER ]; then
         echo "[ INFO ] No Grav admin user details supplied"
     else
-        echo "[ INFO ] Setting up Grav admin user"
+        if [ -e $GRAV_HOME/user/accounts/$ADMIN_USER.yaml ]; then
+            echo "[ INFO ] Grav admin user already exists"
+        else
+            echo "[ INFO ] Setting up Grav admin user"
 
-        bin/plugin login newuser --user=${ADMIN_USER} \
-                                 --password=${ADMIN_PASSWORD-"Pa55word"} \
-                                 --permissions=${ADMIN_PERMISSIONS-"b"} \
-                                 --email=${ADMIN_EMAIL-"admin@domain.com"} \
-                                 --fullname=${ADMIN_FULLNAME-"Administrator"} \
-                                 --title=${ADMIN_TITLE-"SiteAdministrator"}
+            bin/plugin login newuser --user=${ADMIN_USER} \
+                                     --password=${ADMIN_PASSWORD-"Pa55word"} \
+                                     --permissions=${ADMIN_PERMISSIONS-"b"} \
+                                     --email=${ADMIN_EMAIL-"admin@domain.com"} \
+                                     --fullname=${ADMIN_FULLNAME-"Administrator"} \
+                                     --title=${ADMIN_TITLE-"SiteAdministrator"}
+        fi
     fi
-
-    # Set the correct permissions
-    echo "[ INFO ] Setting folder permissions on grav home"
-    chown -R www-data:www-data $GRAV_HOME
 
     # Set the domain name in the nginx config
     if [ -z $DOMAIN ]; then
