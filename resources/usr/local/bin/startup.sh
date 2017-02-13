8fa1edbb649b0e1f7950b3d24bb151a10883e66d
Chris Evans <cae.dev@gmail.com>
2017-02-13 08:36:14 +0000

    Added SSL config to startup. Added responses to automate the acmetool quickstart.

--- resources/usr/local/bin/startup.sh
+++ resources/usr/local/bin/startup.sh
@@ -8,37 +8,55 @@ echo "[ INFO ] Grav home set to" $GRAV_HOME
 cd $GRAV_HOME
 
 
-if [ -e $GRAV_HOME/grav-configured ]; then
-	echo "[ INFO ] Grav already configured"
+# Setup admin user (if supplied)
+if [ -z $ADMIN_USER ]; then
+    echo "[ INFO ] No Grav admin user details supplied"
 else
-
-    if [ -z $ADMIN_USER ]; then
-        echo "[ INFO ] No Grav admin user details supplied"
+    if [ -e $GRAV_HOME/user/accounts/$ADMIN_USER.yaml ]; then
+        echo "[ INFO ] Grav admin user already exists"
     else
-        if [ -e $GRAV_HOME/user/accounts/$ADMIN_USER.yaml ]; then
-            echo "[ INFO ] Grav admin user already exists"
-        else
-            echo "[ INFO ] Setting up Grav admin user"
+        echo "[ INFO ] Setting up Grav admin user"
 
-            sudo -u www-data bin/plugin login newuser --user=${ADMIN_USER} \
-                                                      --password=${ADMIN_PASSWORD-"Pa55word"} \
-                                                      --permissions=${ADMIN_PERMISSIONS-"b"} \
-                                                      --email=${ADMIN_EMAIL-"admin@domain.com"} \
-                                                      --fullname=${ADMIN_FULLNAME-"Administrator"} \
-                                                      --title=${ADMIN_TITLE-"SiteAdministrator"}
-        fi
+        sudo -u www-data bin/plugin login newuser \
+             --user=${ADMIN_USER} \
+             --password=${ADMIN_PASSWORD-"Pa55word"} \
+             --permissions=${ADMIN_PERMISSIONS-"b"} \
+             --email=${ADMIN_EMAIL-"admin@domain.com"} \
+             --fullname=${ADMIN_FULLNAME-"Administrator"} \
+             --title=${ADMIN_TITLE-"SiteAdministrator"}
     fi
+fi
 
-    # Set the domain name in the nginx config
-    if [ -z ${DOMAIN} ]; then
-        echo "[ INFO ] No Domain supplied. Defaulting to localhost"
+# Setup the nginx config, and optionally generate SSL certs
+echo "[ INFO ] Listening on port 80"
+sed -i 's/#listen 80;/listen 80;/g' /etc/nginx/conf.d/default.conf
+
+if [ -z ${DOMAIN} ]; then
+    echo "[ INFO ] No Domain supplied. Not updating server config"
+else
+    if [ "${GENERATE_CERTS}" = true ]; then
+
+        # Generate Let's Encrypt certs
+        echo "[ INFO ] Running acmetool (Let's Encrypt) quickstart"
+        acmetool quickstart > /dev/null
+
+        echo "[ INFO ] Requesting certs for" ${DOMAIN} www.${DOMAIN}
+        acmetool want ${DOMAIN} www.${DOMAIN}
+
+        echo "[ INFO ] Generated certs are:" `ls /var/lib/acme/live/`
+        echo "[ INFO ] Adding SSL settings to Nginx config"
+
+        # Setup SSL in the Nginx config
+        sed -i 's/server_name localhost;/\
+        server_name localhost;\
+        listen 443 ssl;\
+        ssl_certificate \/var\/lib\/acme\/live\/'${DOMAIN}'\/fullchain;\
+        ssl_certificate_key \/var\/lib\/acme\/live\/'${DOMAIN}'\/privkey;/g' /etc/nginx/conf.d/default.conf
+        echo "[ INFO ] Listening on port 443"
     else
-        echo "[ INFO ] Setting server_name to " ${DOMAIN}
-        sed -i 's/server_name localhost/server_name '${DOMAIN}'/g' /etc/nginx/conf.d/default.conf
+        echo "[ INFO ] Setting server_name to" ${DOMAIN} www.${DOMAIN}
+        sed -i 's/server_name localhost/server_name '${DOMAIN} www.${DOMAIN}'/g' /etc/nginx/conf.d/default.conf
     fi
-
-    # Touch file to indicate we're configured for future startups
-    touch /var/www/grav-admin/grav-configured
 fi
 
 # Run nginx as foreground process
